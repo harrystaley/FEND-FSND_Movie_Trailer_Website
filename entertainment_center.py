5e1600af43e6789151ccb5065f5a5f2dda9b5b55
Harry Staley <staleyh@gmail.com>
2016-01-03 09:41:22 -0600

    feat: modularized the HTML, CSS, and JS into separate files and created function to parse the HTML.

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,14 +2,55 @@ import fresh_tomatoes
 import media
 
 toy_story = media.Movie(
-	"Toy Story",
-	"1hr 21min", 
-	"A boy loses his toys and they go on an adventure to get back to the boy.", 
-	"https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
-	"https://www.youtube.com/watch?v=KYz2wyBy3kc")
-#print(toy_story.storyline)
-#toy_story.show_trailer()
+    "Toy Story",
+    "1995",
+    "G",
+    "22 Nov 1995",
+    "81 min",
+    "Animation, Adventure, Comedy",
+    "John Lasseter",
+    "John Lasseter (original story by), Pete Docter (original story by), Andrew Stanton (original story by), Joe Ranft (original story by), Joss Whedon (screenplay), Andrew Stanton (screenplay), Joel Cohen (screenplay), Alec Sokolow (screenplay)",
+    "Tom Hanks, Tim Allen, Don Rickles, Jim Varney",
+    "A cowboy doll is profoundly threatened and jealous when a new spaceman figure supplants him as top toy in a boy"s room.",
+    "English",
+    "USA",
+    "Nominated for 3 Oscars. Another 22 wins & 16 nominations.",
+    "http://ia.media-imdb.com/images/M/MV5BMTgwMjI4MzU5N15BMl5BanBnXkFtZTcwMTMyNTk3OA@@._V1_SX300.jpg",
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-movies = [toy_story]
-print(toy_story.Video_title)
-#fresh_tomatoes.open_movies_page(movies)
+terminator2 = media.Movie(
+    "Terminator 2: Judgment Day",
+    "1991",
+    "R",
+    "03 Jul 1991",
+    "137 min",
+    "Action, Sci-Fi",
+    "James Cameron",
+    "James Cameron, William Wisher Jr.",
+    "Arnold Schwarzenegger, Linda Hamilton, Edward Furlong, Robert Patrick",
+    "A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her young son, John Connor, from a more advanced cyborg, made out of liquid metal.",
+    "English, Spanish",
+    "USA, France",
+    "Won 4 Oscars. Another 20 wins & 22 nominations.",
+    "http://ia.media-imdb.com/images/M/MV5BMTg5NzUwMDU5NF5BMl5BanBnXkFtZTcwMjk2MDA4Mg@@._V1_SX300.jpg",
+    "https://www.youtube.com/watch?v=eajuMYNYtuY")
+
+minions = media.Movie(
+    "Minions",
+    "2015",
+    "PG",
+    "10 Jul 2015",
+    "91 min",
+    "Animation, Comedy, Family",
+    "Kyle Balda, Pierre Coffin",
+    "Brian Lynch",
+    "Sandra Bullock, Jon Hamm, Michael Keaton, Allison Janney",
+    "Minions Stuart, Kevin and Bob are recruited by Scarlet Overkill, a super-villain who, alongside her inventor husband Herb, hatches a plot to take over the world.",
+    "English",
+    "USA",
+    "N/A",
+    "http://ia.media-imdb.com/images/M/MV5BMTg2MTMyMzU0M15BMl5BanBnXkFtZTgwOTU3ODk4NTE@._V1_SX300.jpg",
+    "https://www.youtube.com/watch?v=dVDk7PXNXB8"    )
+
+movies = [toy_story, terminator2, minions]
+fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,43 +1,22 @@
 import webbrowser
 
-class Video():
-	"""
-	This is hte parent class for Movie and Tvshow.
-	"""
-	def __init__(self, video_title, video_duration):
-		self.title = video_title
-		self.duration = video_duration
-		
-class Tvshow(Video):
-	"""
-	This is a child class of video.
-	"""
-	
-	VALID_RATINGS = ["TV Y", "TV Y7", "TV Y7 FV", "TV G", "TV PG", "TV 14", "TV MA"]
-	
-	def __init__(self, video_title, video_duration):
-		Video.__init__(video_title, video_duration)
-		self.arg = arg
-		
+class Movie():
 
-class Movie(Video):
-	""" 
-	This is a child class of Video.
+    VALID_RATINGS = ["G", "PG", "PG-13", "R", "NC-17", "NR"]
 
-	This Class defines the germain informaiton for each movie displayed on my movie website.
-	This rating information includes rating, title, story line, the web address for the movie 
-	poster and the movie trailer on youtube.
-
-	This also includes a call to the web browser to play the video.
-	"""
-
-	VALID_RATINGS = ["G", "PG", "PG-13", "R", "NC-17", "NR"]
-
-	def __init__(self, video_title, video_duration, movie_storyline, poster_image, trailer_youtube):
-		Video.__init__(video_title, video_duration)
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-	
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    def __init__(self, movie_title, movie_year, movie_rating, movie_released, movie_runtime, movie_genre, movie_director, movie_writer, movie_actors, movie_plot, movie_language, movie_country, movie_awards, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.year = movie_year
+        self.rating = movie_rating
+        self.released = movie_released
+        self.duration = movie_runtime
+        self.genre = movie_genre
+        self.director = movie_director
+        self.writer = movie_writer
+        self.actors = movie_actors
+        self.plot = movie_plot
+        self.language = movie_language
+        self.country = movie_country
+        self.awards = movie_awards
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
